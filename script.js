e9a705410eb60d4602eea10fcf7b23c026c9a330
FelipeAAug <145213036+FelipeAAug@users.noreply.github.com>
2023-12-14 12:53:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function somar(a, b){
+
+    return a + b;
+}
+
+let num1 = Number(prompt("Digite o primeiro número: "))
+let num2 = Number(prompt("Digite o segundo número: "))
+
+let resultado = somar(num1, num2);
+alert(`O resultado é: ${resultado}`)
+
+var num = Number(prompt("Digite a base:"))
+var pot = Number(prompt("Digite a potência: "))
+var resultado2 = Math.pow( num , pot)
+
+alert(`O resultado é: ${resultado2}`)
